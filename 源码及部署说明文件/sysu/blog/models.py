1cba750a8c4655a5cfe7ff8a4d080ef71153598b
shrimp525 <zero147852@163.com>
2018-06-12 12:37:47 +0800

    Update models.py

--- 源码及部署说明文件/sysu/blog/models.py
+++ 源码及部署说明文件/sysu/blog/models.py
@@ -17,7 +17,7 @@ class BlogType(models.Model):
     
     def __str__(self):
         return self.type_name
-
+    
 class Blog(models.Model, ReadNumExpandMethod):
     title = models.CharField(max_length=50)
     blog_type = models.ForeignKey(BlogType, on_delete=models.CASCADE)
